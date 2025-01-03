0fe891fb9ab1110419aef92252c2405100715145
berrrchik <berrrchik@mail.ru>
2025-01-03 21:44:25 +0300

    Initial Commit

---
+++ sudokuApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sudokuApp
+//
+//  Created by Анастасия Берчик on 1/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sudokuApp/sudokuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sudokuAppApp.swift
+//  sudokuApp
+//
+//  Created by Анастасия Берчик on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct sudokuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
