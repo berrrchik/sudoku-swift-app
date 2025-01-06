12ca6410e7e741bc06a7dad64e76c6ba6fd893d2
berrrchik <berrrchik@mail.ru>
2025-01-06 22:08:48 +0300

    New file for CellView

--- sudokuApp/SudokuGridView.swift
+++ sudokuApp/SudokuGridView.swift
@@ -56,34 +56,6 @@ struct SudokuGridView: View {
     }
 }
 
-struct CellView: View {
-    let value: Int
-    let isFixed: Bool
-    let isHighlighted: Bool // Подсвечивание строки, столбца, блока
-    let isSelected: Bool // Выбранная ячейка
-    let isSameValue: Bool // Подсветка одинаковых значений
-
-    var body: some View {
-        Text(value == 0 ? "" : "\(value)")
-            .frame(width: 40, height: 40)
-            .background(
-                isSelected ? Color.blue :
-                isSameValue ? Color.blue.opacity(0.7) :
-                isHighlighted ? Color.gray.opacity(0.5) :
-                Color.white
-            )
-            .foregroundColor(.black)
-            .border(Color.black, width: 1)
-            .font(
-                isFixed ? .system(size: 25) : .system(size: 23)
-            )
-            .bold(
-                isFixed == true
-            )
-    }
-}
-
-
 // Предварительный просмотр
 #Preview {
     SudokuGridView(
